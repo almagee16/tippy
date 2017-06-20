8792c857bef092a81780769fd30b3503a63e57e4
Alvin Magee <alvinmagee@Lori-Wus-iPhone.dhcp.thefacebook.com>
2017-06-20 15:25:20 -0700

    Calculate tip for 20%

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +25,17 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * 0.2
+        let total = bill + tip
+    }
+    
 }
 
